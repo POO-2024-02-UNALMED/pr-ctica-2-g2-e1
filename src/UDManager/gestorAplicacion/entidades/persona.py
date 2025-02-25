df92510731585cee2ebb9447bf37029907fb1504
ChristianBustosBetancur <111025495+ChristianBustosBetancur@users.noreply.github.com>
2025-02-24 22:25:11 -0500

    pequño ajuste

--- src/UDManager/gestorAplicacion/entidades/persona.py
+++ src/UDManager/gestorAplicacion/entidades/persona.py
@@ -4,7 +4,7 @@ import pickle
 
 class Persona(ABC):
     def __init__(self, nombre="", apellido="", edad=0, id=0, **kwargs):
-        super().__init__(**kwargs)  # Permite la herencia cooperativa
+        super().__init__(**kwargs)  
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
